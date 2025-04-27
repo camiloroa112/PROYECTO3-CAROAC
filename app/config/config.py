ec01df28abae1e34cfe2fdd73ef90b2b5bfda3d3
camiloroa112 <camiloroa112@gmail.com>
2025-04-27 17:03:45 -0500

    Ajustes en config.py

--- app/config/config.py
+++ app/config/config.py
@@ -3,26 +3,22 @@ from .settings import user, password
 
 # 3rd Party Libraries
 from pathlib import Path
-from dotenv import load_dotenv
-from dotenv import dotenv_values
+from dotenv import load_dotenv, dotenv_values
 
-# Obtención valores de las variables en el archivo .env
-load_dotenv(override = True)
+# Cargar el .env
+load_dotenv(override=True)
 
 # Setting Path where the file is located
-env_path = Path(__file__).resolve().parents[2] / ".env"
+env_path = Path('.env').resolve()
 
 # Reading .env file
-env_vars = dotenv_values(dotenv_path = env_path)
+env_vars = dotenv_values(dotenv_path=env_path)
 
 # Placing info into a dictionary
-env_credentials = {key:value for key, value in env_vars.items()}
+env_credentials = {key: value for key, value in env_vars.items()}
 
 class Config:
     """Colocación de las credenciales en variables para acceder a la base tablas."""
-    
-    # Obtaining Secret_Key
-    SECRET_KEY = env_credentials['SECRET_KEY']
 
-    # Reading DB
+    SECRET_KEY = env_credentials['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = env_credentials['SQLALCHEMY_DATABASE_URI'] + f'{user}:{password}@localhost:3306/heladeria'
