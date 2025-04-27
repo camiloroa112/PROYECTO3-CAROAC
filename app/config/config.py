1f041f642189528d7f5e1606f6e9df43283e0e8b
camiloroa112 <camiloroa112@gmail.com>
2025-04-27 17:17:40 -0500

    Modificación de las configuraciones de acceso.

--- app/config/config.py
+++ app/config/config.py
@@ -2,23 +2,14 @@
 from .settings import user, password
 
 # 3rd Party Libraries
-from pathlib import Path
-from dotenv import load_dotenv, dotenv_values
+import os
+from dotenv import load_dotenv
 
 # Cargar el .env
-load_dotenv(override=True)
-
-# Setting Path where the file is located
-env_path = Path('.env').resolve()
-
-# Reading .env file
-env_vars = dotenv_values(dotenv_path=env_path)
-
-# Placing info into a dictionary
-env_credentials = {key: value for key, value in env_vars.items()}
+load_dotenv(override = True)
 
 class Config:
     """Colocación de las credenciales en variables para acceder a la base tablas."""
 
-    SECRET_KEY = env_credentials['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = env_credentials['SQLALCHEMY_DATABASE_URI'] + f'{user}:{password}@localhost:3306/heladeria'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
